10a696b2fc2fe786e10d48a44d5a0d7d4b0affa8
menachem <AM0556727820@GMAIL.COM>
2024-10-10 10:16:56 +0300

    with post model

---
+++ src/models/postModel.ts
@@ -0,0 +1,53 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IComment {
+  //המבנה של הערה
+  content: string; //התוכן של הערה
+  author: Types.ObjectId; //הערה מקושרת למזהה של היוצר שלה
+  createdAt: Date; //תאריך יצירת ההודעה
+}
+
+export interface IPost extends Document {
+  _id: Types.ObjectId;
+  title: string;
+  content: string;
+  author: Types.ObjectId; //הערה מקושרת למזהה של היוצר שלה
+  comments: IComment[];
+}
+
+const CommentSchema = new Schema<IComment>({
+  //סכמה של הערה (סוג של מבנה שמוכר למונגו)
+  content: {
+    type: String,
+    required: [true, "content is required!"],
+  },
+  author: {
+    type: Schema.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    required: [true, "date is required!"],
+  },
+});
+
+const PostSchema = new Schema<IPost>({
+  title: {
+    type: String,
+    required: [true, "title is required!"],
+    minlength: [4, "title to be at last 4 chars"],
+  },
+  content: {
+    type: String,
+    required: [true, "content is required!"],
+  },
+  author: {
+    type: Schema.ObjectId,
+    ref: "User",
+  },
+  comments: {
+    type: [CommentSchema], //פוסט מכיל מערך של הערות
+  },
+});
+
+export default mongoose.model<IPost>("Post", PostSchema);
